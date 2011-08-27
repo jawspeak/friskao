c8d0ec111ace4f8a1a32ad1435a0d2e2a9665512
Jonathan Andrew Wolter <jaw@squareup.com>
2011-08-27 11:14:43 -0700

    now with wheelbarrow, that needs to have 1) draggable and 2) capture area at the top to get points

--- main.lua
+++ main.lua
@@ -14,12 +14,12 @@ physics.setGravity(0, 6)
 
 display.setStatusBar( display.HiddenStatusBar )
 
+local wheelbarrow = display.newImage( "wheelbarrow.png", 0, 200 )
+physics.addBody( wheelbarrow, "static", { friction=0.3, bounce=0.1, shape={-40,-30, 40,-30, 40,20, -40,20}})
+
+
 local ground = display.newImage( "grass.png", 0, 300, true )
 physics.addBody( ground, "static", { friction=0.5, bounce=0.9, shape={-240,-10, 240,-10, 240,10, -240,10}})
-
---local leftside = display.newSquare(0,0,100,100)
---physics.addBody( ground, "static", { friction=0.5, bounce=0.9, shape={-240,-10, 240,-10, 240,10, -240,10}})
- 
 
 function randomFace()
 	choice = math.random(100)
@@ -39,21 +39,20 @@ local randomBall = function()
 	local ball
 	ball = display.newImage(randomFace())
 	ball.x = 40 + math.random( 380 ); ball.y = -40
-	physics.addBody( ball, { density=0.6, friction=0.6, bounce=0.6, radius=29 } )
+	physics.addBody( ball, { density=0.6, friction=0.6, bounce=0.9, radius=29 } )
 	ball.angularVelocity = math.random(800) - 400
 	ball.isSleepingAllowed = false
 
 	balls[#balls + 1] = ball
 end
-
-timer.performWithDelay( 750, randomBall, 24 )
+timer.performWithDelay( 750, randomBall, 50 )
 
 sounds = {}
 sounds[0] = audio.loadSound("freesoundpongblip1_ogg.ogg");
 sounds[1] = audio.loadSound("freesoundpongblip2_ogg.ogg");
 sounds[2] = audio.loadSound("freesoundpongblip3_ogg.ogg");
 local function onCollision( event )
-	if ( event.phase == "began" ) then
+	if (event.phase == "began") then
 		audio.play(sounds[math.random(3)]);
 	end
 end
