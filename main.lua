8c13437875054d409603dc2e67f8d2cbe98654ec
Kao Félix <kcfelix@gmail.com>
2011-08-27 11:54:36 -0700

    now it collects for real, promise.

--- main.lua
+++ main.lua
@@ -15,6 +15,7 @@ physics.setGravity(0, 6)
 display.setStatusBar( display.HiddenStatusBar )
 
 local wheelbarrow = display.newImage( "wheelbarrow.png", 0, 200 )
+wheelbarrow.name = "wheelbarrow"
 physics.addBody( wheelbarrow, "static", { friction=0.3, bounce=0.1, shape={-40,-30, 40,-30, 40,20, -40,20}})
 
 
@@ -42,6 +43,7 @@ local randomBall = function()
 	physics.addBody( ball, { density=0.6, friction=0.6, bounce=0.9, radius=29 } )
 	ball.angularVelocity = math.random(800) - 400
 	ball.isSleepingAllowed = false
+        ball.name = "head"
 
 	balls[#balls + 1] = ball
 end
@@ -51,9 +53,40 @@ sounds = {}
 sounds[0] = audio.loadSound("freesoundpongblip1_ogg.ogg");
 sounds[1] = audio.loadSound("freesoundpongblip2_ogg.ogg");
 sounds[2] = audio.loadSound("freesoundpongblip3_ogg.ogg");
+powerUpSound = audio.loadSound("powerup.wav")
+
+local function getWheelbarrow( collisionEvent )
+   if (collisionEvent.object1.name == "wheelbarrow") then
+      return collisionEvent.object1
+   elseif ( collisionEvent.object2.name == "wheelbarrow") then 
+      return collisionEvent.object2
+   end
+   return nil
+end
+
+local function getHead( collisionEvent )
+   if (collisionEvent.object1.name == "head") then 
+      return collisionEvent.object1
+   elseif (collisionEvent.object2.name == "head") then
+      return collisionEvent.object2
+   end
+   return nil
+end
+
 local function onCollision( event )
-	if (event.phase == "began") then
-		audio.play(sounds[math.random(3)]);
-	end
+   if (event.phase == "ended") then
+      wheelbarrow = getWheelbarrow(event)
+
+      if ( wheelbarrow ) then
+         head = getHead(event)
+         head:removeSelf()
+         audio.play(powerUpSound)
+         return
+      end
+   end
+
+   if (event.phase == "began") then
+      audio.play(sounds[math.random(3)]);
+   end
 end
 Runtime:addEventListener( "collision", onCollision )
